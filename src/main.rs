a74d7ade2255c9fdf04b2836140328a89d743425
JethroNatividad <natividad.jet@gmail.com>
2023-12-27 23:18:04 +0800

    write problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// Calculate gallons of paint needed to paint the ceiling of a
+// room. Prompt for the length and width, and assume one
+// gallon covers 350 square feet. Display the number of gallons
+// needed to paint the ceiling as a whole number
+
+// Remember, you can’t buy a partial gallon of paint. You must
+// round up to the next whole gallon.
+
+// n of gallons to paint the ceiling of lxw room, where a gallon covers 350 sqft, round up the gallon.
+
+// Inputs: Length, Width
+// Process: ceiling sqft = lxw, gallons = round_up(ceiling sqft / gallon sqft).
+// Output: You will need to purchase {n} gallon/s of paint to cover {ceiling sqft} square feet.
+
+
 fn main() {
     println!("Hello, world!");
 }
